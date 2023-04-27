12d9e68c339f0fd2ba91db53baae1400c91763e8
Juan <elquesififa@gmail.com>
2023-04-27 09:43:57 -0500

    finish

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const emailMenu = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const emailArrow = document.querySelector('#email-arrow')
 
 const mobileMenu = document.querySelector('.mobile-menu')
 const burguerMenu = document.querySelector('.icon-menu')
 
 const shoppingMenu = document.querySelector('.shopping-cart')
 const shoppingCart = document.querySelector('.shopping-cart-icon')
+const shoppingArrow = document.querySelector('#shopping-arrow')
 
-const cardsContainer = document.querySelector('.cards-container')
+const cardsContainer = document.querySelector('.cards-container'
+)
 
 let productList = []
 productList.push({name: 'guitar', price: 120, image: 'https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'})
@@ -17,34 +20,49 @@ productList.push({name: 'guitar', price: 120, image: 'https://images.pexels.com/
 productList.push({name: 'guitar', price: 120, image: 'https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'})
 
 productList.push({name: 'guitar', price: 120, image: 'https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'})
+renderProducts(productList)
+
+
+const productDetail =document.querySelector('.product-detail');
+
+const asideClose = document.querySelector('.product-detail-close')
+asideClose.addEventListener('click', toggleProductDetail)
+
+
+
 
 // function Product(img, name, price) {
 //   this.img = img;
 //   this.name = name;
 //   this.price = price;
 //   };
+emailMenu.addEventListener('click', toggleEmailMenu)
+emailArrow.addEventListener('click', toggleEmailMenu)
+burguerMenu.addEventListener('click', toggleMobileMenu)
+shoppingCart.addEventListener('click', toggleShoppingMenu)
+shoppingArrow.addEventListener('click', toggleShoppingMenu)
+
 function renderProducts(arr) {
-  
-
-
 for (i of arr) {
   const productCard = document.createElement('div');
   productCard.classList.add('product-card');
 
   
   const productImg = document.createElement('img');
-  productImg.setAttribute('src', product.image);
+  productImg.setAttribute('src', i.image);
 
   const productInfo = document.createElement('div');
   productInfo.classList.add('product-info');
 
   const productInfoDiv = document.createElement('div');
 
-  const productInfoName = document.createElement('p');
-  productInfoName.innerText = product.name
-
   const productInfoPrice = document.createElement('p');
-  productInfoPrice.innerText= '$' + product.price
+
+  productInfoPrice.innerText= '$' + i.price
+  const productInfoName = document.createElement('p');
+  productInfoName.innerText = i.name
+
+  
 
   const productInfoFigure = document.createElement('figure');
 
@@ -53,63 +71,80 @@ for (i of arr) {
 
   productCard.append(productImg, productInfo)
   productInfo.append(productInfoDiv, productInfoFigure)
-  productInfoDiv.append(productInfoName, productInfoPrice)
+  productInfoDiv.append(productInfoPrice,productInfoName)
   productInfoFigure.append(productInfoCartIcon)
   cardsContainer.append(productCard)
   
+ 
+  productCard.addEventListener('click', toggleProductDetail)
 }
 
 console.log(productList)}
 
-emailMenu.addEventListener('click', toggleEmailMenu)
-burguerMenu.addEventListener('click', toggleMobileMenu)
-shoppingCart.addEventListener('click', toggleShoppingMenu)
+
 
 
 function toggleEmailMenu(event) {
   event.preventDefault();
+  if (!productDetail.classList.contains('inactive')) {
+    productDetail.classList.toggle('inactive');
+  }
   if (!shoppingMenu.classList.contains('inactive')) {
-    console.log('no contiene')
     shoppingMenu.classList.toggle('inactive');
-    
   }
   desktopMenu.classList.toggle('inactive');
+  
 }
 function toggleMobileMenu() {
   if (!shoppingMenu.classList.contains('inactive')) {
 
     shoppingMenu.classList.toggle('inactive');
   }
+  if (!productDetail.classList.contains('inactive')) {
+
+    productDetail.classList.toggle('inactive');
+  }
   mobileMenu.classList.toggle('inactive');
 }
   
 function toggleShoppingMenu(){
-  if (!desktopMenu.classList.contains('inactive')) {
-    desktopMenu.classList.toggle('inactive');
-  }
+  
   if (!mobileMenu.classList.contains('inactive')) {
     mobileMenu.classList.toggle('inactive');
   } 
+  if (!productDetail.classList.contains('inactive')) {
+    productDetail.classList.toggle('inactive');
+  } 
+  if (!desktopMenu.classList.contains('inactive')) {
+    desktopMenu.classList.toggle('inactive');
+  }
 
   shoppingMenu.classList.toggle('inactive');
 }
-// function newProductadd(img, nombre, precio){
-//   let product = new Product(img, nombre, precio);
-//   productList.push(product)
+function toggleProductDetail(){
+  if (!shoppingMenu.classList.contains('inactive')) {
 
-// }
+    shoppingMenu.classList.toggle('inactive');
+  }
+  if (!desktopMenu.classList.contains('inactive')) {
 
-/* <div class="product-card">
-          <img src="https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="product">
-          <div class="product-info">
-            <div>
-              <p>120,00$</p>
-              <p>Product</p>
-            </div>
-            <figure>
-              <img src="./Platzi_YardSale_Icons/bt_add_to_cart.svg" alt="">
-            </figure>
-          </div> */
-
+    desktopMenu.classList.toggle('inactive');
+  }
   
+  productDetail.classList.toggle('inactive')
+}
 
+/* <aside class="product-detail inactive">
+      <div class="product-detail-image-container">
+         <img src="./Platzi_YardSale_Icons/icon_close.png" alt="close" class="product-detail-close">
+         <img src="https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Guitar" class="aside-product-image"></div>
+         <div class="product-info-aside">
+           <p>$35,00</p>
+           <p>Bike</p>
+           <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+           <button class="aside-button add-to-cart-button">
+             <img src="./Platzi_YardSale_Icons/bt_add_to_cart.svg" alt="add to cart">
+             Add to cart
+           </button>
+       </div>
+     </aside> */
